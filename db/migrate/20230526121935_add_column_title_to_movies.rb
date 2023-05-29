f935743cbe35566d394fce352ab705ceaaa50349
Katrina Gherzeddine <katgherzeddine@gmail.com>
2023-05-29 16:11:34 +0200

    exo

---
+++ db/migrate/20230526121935_add_column_title_to_movies.rb
@@ -0,0 +1,4 @@
+class AddColumnTitleToMovies < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
---
+++ db/migrate/20230526123439_add_ttitle_to_movies.rb
@@ -0,0 +1,5 @@
+class AddTtitleToMovies < ActiveRecord::Migration[7.0]
+  def change
+    add_column :movies, :title, :string
+  end
+end
